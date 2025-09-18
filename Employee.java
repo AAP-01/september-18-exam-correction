2153607af6caa79d84e6b54a48a014f35c236675
John Benedict Castroverde <jbthememer3@gmail.com>
2025-09-18 13:43:40 -0400

    original exam answers

--- Employee.java
+++ Employee.java
@@ -31,13 +31,24 @@ public class Employee{
      */
     public boolean canDrive(){
         int age = calculateAge(2025);
+        if (age >= 16) then{
+            System.out.println(fullname + " can drive.");
+        }else{
+            int yearsUntilSisxteen = 16 - age;
+            System.out.println(fullname + " must wait " + yearsUntilSixteen + " more years before they can drive.");
+        }
+    
         // to be completed
     }
-
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
     private double calculatePay(){
+        1. Get values of unpaidHouts and hourlyWage
+        2. Set taxRate to 30%
+        3. Set Gross to unpaidHours * hourlyWage
+        4. Set Deductions to Gross * taxRate
+        5. Set netSalary to Gross - Deductions
         // to be completed
     }
 
@@ -45,6 +56,9 @@ public class Employee{
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
+        System.out.println(fullname + " has received a wire transfer of " + netSalary + " $CAD.");
+        
+    unpaidHours = 0.0;
         // to be completed
     }
 }
